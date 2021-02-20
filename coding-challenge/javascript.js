34cc7c45e06f93f46b0058795c3e86b924a47d51
Ralph Ian Santos <sralphian029@gmail.com>
2021-02-20 10:31:05 +0800

    javascript.js

--- coding-challenge/javascript.js
+++ coding-challenge/javascript.js
@@ -1,15 +1,15 @@
 console.log("I am a Javascript file");
 
 var victorMass = 65.77;
-var victorHeight = 1.79832;
-var johnMass = 62.1422;
+var victorHeight = 1.80;
+var johnMass = 62.14;
 var johnHeight = 1.74;
 
 var victorBMI = victorMass / (victorHeight * victorHeight);
 var johnBMI = johnMass / (johnHeight * johnHeight);
 
-console.log(victorBMI);
-console.log(johnBMI);
+console.log(victorBMI = Math.round(victorBMI * 100) / 100);
+console.log(johnBMI = Math.round(johnBMI * 100) / 100);
 
 var compareBMI = victorBMI > johnBMI;
 
